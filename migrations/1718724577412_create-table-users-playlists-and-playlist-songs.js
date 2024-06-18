e9a5d7adbc2a4a43c6a5647a70923bcb6c457dcb
Yogi Anggara <yogianggaraaa@gmail.com>
2024-06-19 05:37:24 +0700

    feat(migrate): create users, playlists, and playlist_songs

---
+++ migrations/1718724577412_create-table-users-playlists-and-playlist-songs.js
@@ -0,0 +1,76 @@
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable('users', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+      unique: true,
+    },
+    username: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    password: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    fullname: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+  pgm.createTable('playlists', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+      unique: true,
+    },
+    name: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'users',
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    },
+  });
+  pgm.createTable('playlist_songs', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+      unique: true,
+    },
+    playlist_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'playlists',
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    },
+    song_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'songs',
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable('playlist_songs');
+  pgm.dropTable('playlists');
+  pgm.dropTable('users');
+};
