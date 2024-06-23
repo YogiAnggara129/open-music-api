f77b79d6563e74ad1326ae7172de348389b9933d
Yogi Anggara <yogianggaraaa@gmail.com>
2024-06-23 19:00:23 +0700

    feat(migration): drop created at and updated at in albums and songs table

---
+++ migrations/1718750347248_drop-created-at-and-updated-at-in-albums-and-songs.js
@@ -0,0 +1,41 @@
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.dropColumns('albums', ['created_at', 'updated_at']);
+  pgm.dropColumns('songs', ['created_at', 'updated_at']);
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.addColumns('albums', {
+    created_at: {
+      type: 'TIMESTAMP',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+    updated_at: {
+      type: 'TIMESTAMP',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  });
+  pgm.addColumns('songs', {
+    created_at: {
+      type: 'TIMESTAMP',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+    updated_at: {
+      type: 'TIMESTAMP',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  });
+};
