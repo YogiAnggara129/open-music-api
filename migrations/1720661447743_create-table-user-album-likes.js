3b87450a75d9ceef8f8f81f8a88cae8f267052ea
Yogi Anggara <yogianggaraaa@gmail.com>
2024-07-11 09:57:17 +0700

    feat(migrate): create table user_album_likes

---
+++ migrations/1720661447743_create-table-user-album-likes.js
@@ -0,0 +1,37 @@
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable('user_album_likes', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+      unique: true,
+    },
+    user_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'users',
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    },
+    album_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'albums',
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable('user_album_likes');
+};
